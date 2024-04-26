fd03e8dd102ed6f8c640f6ca5e19002ec951e693
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-25 18:17:27 -0700

    styling of the background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resource"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -20,8 +23,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Make the most recently drawn screen visible.
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
     
 if __name__ == "__main__":
     # Make a game instance, and run the game.
