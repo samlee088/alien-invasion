31a6a09fa4ee45da1deb7d760d7599d64e93652b
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-26 23:18:09 -0700

    update with being able to render and display a level

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
             self.game_active = True
 
             # Get rod of any remaining bullets and aliens.
@@ -141,12 +142,17 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create a new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
