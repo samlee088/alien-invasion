b350523b9e4ae58f97d4f37c1c5a178170082d0e
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-26 19:41:52 -0700

    alien fleet is now able to drop and switch directions

--- alien_invasion.py
+++ alien_invasion.py
@@ -111,8 +111,23 @@ class AlienInvasion:
         self.aliens.add(new_alien)
 
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet"""
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_directions()
+                break
+
+
+    def _change_fleet_directions(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
