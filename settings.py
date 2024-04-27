55e8e4756f1f1c5f29b2f43be08c7c391ee56b89
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-26 23:03:48 -0700

    rendering of scoreboard

--- settings.py
+++ settings.py
@@ -22,11 +22,15 @@ class Settings:
         # Alien settings
         self.alien_speed = 10
         self.fleet_drop_speed = 10
+
         # fleet direction of 1 represents right: -1 represents left.
         self.fleet_direction = 1
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -43,9 +47,11 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
 
         
